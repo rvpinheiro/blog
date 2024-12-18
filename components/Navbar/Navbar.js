29fedb387cf41e9e7ecc09cdbe6c8e3c732c0d2f
Rui Pinheiro <rui.pinheiro@askblue.pt>
2024-12-18 14:39:52 +0000

    Responsive layout media queries

--- components/Navbar/Navbar.js
+++ components/Navbar/Navbar.js
@@ -1,9 +1,30 @@
+'use client'
+
+import { useState, useEffect } from 'react';
 import Link from 'next/link';
 import styles from './Navbar.module.css';
 
 export default function Navbar() {
+    const [scrolling, setScrolling] = useState(false);
+
+    const handleScroll = () => {
+        if (window.scrollY > 10) {
+            setScrolling(true);
+        } else {
+            setScrolling(false);
+        }
+    };
+
+    useEffect(() => {
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+
     return (
-        <nav className={styles.navbar}>
+        <nav className={`${styles.navbar} ${scrolling ? styles.scrolled : ''}`}>
             <div>
                 <p className={styles['blog-text']}>BLOG</p>
             </div>
